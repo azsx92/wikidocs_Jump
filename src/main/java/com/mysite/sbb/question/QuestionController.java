a300eb1827278b8f4235500cea2503eb874c948b
azsx92 <azsx92@gmail.com>
2023-07-08 08:50:13 +0900

    3-04 reply count

--- src/main/java/com/mysite/sbb/question/QuestionController.java
+++ src/main/java/com/mysite/sbb/question/QuestionController.java
@@ -26,11 +26,12 @@ public class QuestionController {
         return "question_list";
     }
 
-    @GetMapping(value = "/detail/{id}")
-    public String detail(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
+    @GetMapping(value = "/detatil/{id}")
+    public String detatil(Model model, @PathVariable("id") Integer id, AnswerForm answerForm) {
         Question question = this.questionService.getQuestion(id);
         model.addAttribute("question", question);
-        return "question_detail";
+        return "question_detatil";
+
     }
 
     @GetMapping("/create")
